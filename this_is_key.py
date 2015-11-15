44ab1709c9a6ac6153efe30b72fbd8ea2e2ae05d
psandn <philipsander@icloud.com>
2015-11-15 17:18:34 +0100

    python
    just some code to test the command git add -A

---
+++ this_is_key.py
@@ -0,0 +1,12 @@
+__author__ = 'philip'
+
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+# Add your code below!
+for item in webster:
+    print webster[item]
